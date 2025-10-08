1875a40c95f597e802d98941801a2b059765663d
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2025-10-08 16:42:44 +0200

    Complete coprocessor implementation

--- TP2_etudiant/vci_gcd_coprocessor.cpp
+++ TP2_etudiant/vci_gcd_coprocessor.cpp
@@ -46,13 +46,14 @@ namespace soclib
             r_pktid("r_pktid"), r_opa("r_opa"), r_opb("r_opb"), m_segment(mt.getSegment(index)), p_resetn("resetn"),
             p_clk("clk"), p_vci("vci")
         {
+            // Note: https://largo.lip6.fr/trac/sesi-soclib/wiki/SoclibCourseTp1#a3.1EcrituredumodèleCABAducoprocesseur
             SC_METHOD(transition);
             dont_initialize();
-            sensitive << ; // TODO: A COMPLETER
+            sensitive << p_clk.pos();
 
             SC_METHOD(genMoore);
             dont_initialize();
-            sensitive << ; // TODO: A COMPLETER
+            sensitive << p_clk.neg();
         }
 
         ////////////////////////////
@@ -63,8 +64,8 @@ namespace soclib
         {
             if (!p_resetn.read())
             {
-                r_vci_fsm = ; // TODO: A COMPLETER
-                r_exe_fsm = ; // TODO: A COMPLETER
+                r_vci_fsm = VCI_GET_CMD;
+                r_exe_fsm = EXE_IDLE;
                 return;
             }
 
@@ -79,23 +80,25 @@ namespace soclib
             {
             case EXE_IDLE:
                 if (r_vci_fsm.read() == VCI_RSP_START)
-                    r_exe_fsm = ; // TODO: A COMPLETER
+                    r_exe_fsm = EXE_COMPARE;
                 break;
             case EXE_COMPARE:
                 if (r_opa.read() < r_opb.read())
-                    r_exe_fsm = ; // TODO: A COMPLETER
+                    r_exe_fsm = EXE_DECB;
                 else if (r_opb.read() < r_opa.read())
-                    r_exe_fsm = ; // TODO: A COMPLETER
+                    r_exe_fsm = EXE_DECA;
                 else
-                    r_exe_fsm = ; // TODO: A COMPLETER
+                    r_exe_fsm = EXE_IDLE;
                 break;
             case EXE_DECA:
-                r_opa     = ; // TODO: A COMPLETER
-                r_exe_fsm = ; // TODO: A COMPLETER
+                // In this state, we get the difference (r_opa - r_opb)
+                r_opa     = r_opa.read() - r_opb.read();
+                r_exe_fsm = EXE_COMPARE;
                 break;
             case EXE_DECB:
-                r_opb     = ; // TODO: A COMPLETER
-                r_exe_fsm = ; // TODO: A COMPLETER
+                // In this state, we get the difference (r_opb - r_opa)
+                r_opb     = r_opb.read() - r_opa.read();
+                r_exe_fsm = EXE_COMPARE;
                 break;
             } // end switch exe-fsm
 
@@ -113,31 +116,35 @@ namespace soclib
                            (p_vci.cmd.read() != vci_param::CMD_STORE_COND) && (m_segment.contains(address)) &&
                            "illegal command received by the GCD coprocessor");
                     // store the VCI command in registers
-                    r_srcid = ; // TODO: A COMPLETER
-                    r_trdid = ; // TODO: A COMPLETER
-                    r_pktid = ; // TODO: A COMPLETER
+                    // Note: Reviser
+                    r_srcid = p_vci.srcid.read(); // identifiant de l’initiateur → sert au routage de la réponse.
+                    r_trdid = p_vci.trdid.read(); // identifiant de transaction → différencier plusieurs transactions
+                                                  // d’un même initiateur.
+                    r_pktid = p_vci.pktid.read(); // identifiant de paquet → permet de suivre les paquets
+
                     // test the command
                     if ((p_vci.cmd.read() == vci_param::CMD_READ) && (cell == GCD_OPA))
                     {
-                        r_vci_fsm = ; // TODO: A COMPLETER
+                        r_vci_fsm = VCI_RSP_RESULT;
                     }
                     else if ((p_vci.cmd.read() == vci_param::CMD_READ) && (cell == GCD_STATUS))
                     {
-                        r_vci_fsm = ; // TODO: A COMPLETER
+                        r_vci_fsm = VCI_RSP_STATUS;
                     }
                     else if ((p_vci.cmd.read() == vci_param::CMD_WRITE) && (cell == GCD_OPA))
                     {
-                        r_opa     = ; // TODO: A COMPLETER
-                        r_vci_fsm = ; // TODO: A COMPLETER
+                        // We get the value from the WDATA signal of the VCI interface since it is a WRITE command
+                        r_opa     = p_vci.wdata.read();
+                        r_vci_fsm = VCI_RSP_OPA;
                     }
                     else if ((p_vci.cmd.read() == vci_param::CMD_WRITE) && (cell == GCD_OPB))
                     {
-                        r_opb     = ; // TODO: A COMPLETER
-                        r_vci_fsm = ; // TODO: A COMPLETER
+                        r_opb     = p_vci.wdata.read();
+                        r_vci_fsm = VCI_RSP_OPB;
                     }
                     else if ((p_vci.cmd.read() == vci_param::CMD_WRITE) && (cell == GCD_START))
                     {
-                        r_vci_fsm = ; // TODO: A COMPLETER
+                        r_vci_fsm = VCI_RSP_START;
                     }
                     else
                     {
@@ -152,48 +159,63 @@ namespace soclib
             case VCI_RSP_STATUS:
             case VCI_RSP_RESULT:
                 if (p_vci.rspack.read())
-                    r_vci_fsm = ; // TODO: A COMPLETER
+                    // Go back to the initial state to fetch a new command
+                    r_vci_fsm = VCI_GET_CMD;
                 break;
             } // end switch vci_fsm
         } // end transition()
 
         ////////////////////////////////////////
+        // Rappel: la fonction genMoore() est sensible au front descendant du port d'entrée CK, et permet de calculer la
+        // valeur des ports de sortie qui ne dépendent que des valeurs stockées dans les registres.
         template <typename vci_param> void VciGcdCoprocessor<vci_param>::genMoore()
         {
-            // sorties ind�pendantes de l'�tat de l'automate
-            p_vci.rsrcid = ; // TODO: A COMPLETER
-            p_vci.rtrdid = ; // TODO: A COMPLETER
-            p_vci.rpktid = ; // TODO: A COMPLETER
-            p_vci.rerror = ; // TODO: A COMPLETER
-            p_vci.reop   = ; // TODO: A COMPLETER
+            // sorties indépendantes de l'état de l'automate
+            // These are the response signals of the VCI interface
+            p_vci.rsrcid = r_srcid; // rappel du SRCID pour retourner vers l’initiateur correct
+            p_vci.rtrdid = r_trdid; // rappel du TRDID pour rattacher la RSP à la bonne CMD
+            p_vci.rpktid = r_pktid; // Response packet ID
+            p_vci.rerror = false;   // indication d’erreur (pas d’erreur)
+            p_vci.reop   = true;    // fin de paquet de réponse
 
             switch (r_vci_fsm)
             {
+            // Waiting for and accepting new VCI commands from initiators
             case VCI_GET_CMD:
-                p_vci.cmdack = ; // TODO: A COMPLETER
-                p_vci.rspval = ; // TODO: A COMPLETER
-                p_vci.rdata  = ; // TODO: A COMPLETER
+                p_vci.cmdack = true;  // Ready to accept commands
+                p_vci.rspval = false; // No response ready
+                p_vci.rdata  = 0;     // No data to return
                 break;
+            // Acknowledging write commands (no data to return)
             case VCI_RSP_OPA:
             case VCI_RSP_OPB:
             case VCI_RSP_START:
-                p_vci.cmdack = ; // TODO: A COMPLETER
-                p_vci.rspval = ; // TODO: A COMPLETER
-                p_vci.rdata  = ; // TODO: A COMPLETER
+                p_vci.cmdack = false; // Not accepting new commands
+                p_vci.rspval = true;  // Valid response ready
+                p_vci.rdata  = 0;     // No actual data
                 break;
+            // Responding to status read commands
             case VCI_RSP_STATUS:
-                p_vci.cmdack = ; // TODO: A COMPLETER
-                p_vci.rspval = ; // TODO: A COMPLETER
-                p_vci.rdata  = ; // TODO: A COMPLETER
+                p_vci.cmdack = false;
+                p_vci.rspval = true;
+                p_vci.rdata  = (r_exe_fsm.read() != EXE_IDLE);
+                /*Returns 1 if GCD calculation is running (exe_fsm ≠ EXE_IDLE)
+Returns 0 if GCD calculation is finished (exe_fsm = EXE_IDLE)*/
                 break;
+
+            /*Purpose: Responding to result read requests:
+
+Only sends valid response when calculation is complete (exe_fsm == EXE_IDLE)
+Returns the GCD result stored in r_opa
+If calculation is still running, rspval = false (no response yet)*/
             case VCI_RSP_RESULT:
-                p_vci.cmdack = ; // TODO: A COMPLETER
-                p_vci.rspval = ; // TODO: A COMPLETER
-                p_vci.rdata  = ; // TODO: A COMPLETER
+                p_vci.cmdack = false;
+                p_vci.rspval = (r_exe_fsm.read() == EXE_IDLE);
+                p_vci.rdata  = r_opa.read();
             }
         } // end genMoore()
 
-        template class VciGcdCoprocessor < soclib::caba::VciParams < ; // TODO: A COMPLETER> >;
+        template class VciGcdCoprocessor<soclib::caba::VciParams<4, 4, 32, 1, 1, 1, 12, 1, 1, 1>>;
 
     } // namespace caba
 } // namespace soclib
