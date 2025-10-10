672aba6bf139b750c673664655ed8c6e7df3de29
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2025-10-10 15:13:08 +0200

    Create multiprocessor top file

--- TP2_etudiant/tp2_multi_top.cpp
+++ TP2_etudiant/tp2_multi_top.cpp
@@ -6,10 +6,15 @@
 #include "vci_gcd_master.h"
 #include "vci_param.h"
 #include "vci_signals.h"
+#include "vci_vgsb.h" // VCI VGSB bus
 
+// We have 3 masters and 3 coprocessors, so we need 3 different base addresses
 // Base address and size of the GCD memory-mapped peripheral
-#define GCD_BASE 0x00000000
-// 4 registers of 4 bytes each = 16 bytes
+#define GCD0_BASE 0x00000000
+#define GCD1_BASE 0x00000010
+#define GCD2_BASE 0x00000020
+// Every master has the same size, so we only need to define it one time
+// 4 registers of 4 bytes each = 16 bytes or 0x10
 #define GCD_SIZE (1 << 4)
 
 int sc_main(int argc, char* argv[])
@@ -46,7 +51,9 @@ int sc_main(int argc, char* argv[])
     // Mapping Table
     //////////////////////////////////////////////////////////////////////////
     MappingTable maptab(32, IntTab(8), IntTab(8), 0x03000000);
-    maptab.add(soclib::common::Segment("GCD", GCD_BASE, GCD_SIZE, IntTab(0), true));
+    maptab.add(soclib::common::Segment("GCD0", GCD0_BASE, GCD_SIZE, IntTab(0), true));
+    maptab.add(soclib::common::Segment("GCD1", GCD1_BASE, GCD_SIZE, IntTab(1), true));
+    maptab.add(soclib::common::Segment("GCD2", GCD2_BASE, GCD_SIZE, IntTab(2), true));
     std::cout << std::endl << maptab << std::endl;
 
     //////////////////////////////////////////////////////////////////////////
@@ -54,7 +61,15 @@ int sc_main(int argc, char* argv[])
     //////////////////////////////////////////////////////////////////////////
     sc_clock signal_clk("signal_clk", sc_time(1, SC_NS), 0.5);
     sc_signal<bool> signal_resetn("signal_resetn");
-    VciSignals<vci_param> signal_vci("signal_vci");
+    // Since we are now using multiple initiators and multiple targets, we need to define multiple signals
+    // Initiators/Masters
+    VciSignals<vci_param> signal_vci_m0("signal_vci_m0");
+    VciSignals<vci_param> signal_vci_m1("signal_vci_m1");
+    VciSignals<vci_param> signal_vci_m2("signal_vci_m2");
+    // Targets/Slaves
+    VciSignals<vci_param> signal_vci_s0("signal_vci_s0");
+    VciSignals<vci_param> signal_vci_s1("signal_vci_s1");
+    VciSignals<vci_param> signal_vci_s2("signal_vci_s2");
 
     //////////////////////////////////////////////////////////////////////////
     // Components
@@ -62,22 +77,55 @@ int sc_main(int argc, char* argv[])
     /* Note: Dans ce TP, on utilisera *un seul niveau d'indexation* (cf. int_tab.h),
     alors que SoCLib utilise des index composites. */
 
-    // VciGcdMaster(sc_module_name insname, const IntTab& index, const MappingTable& mt, const int seed,
-    // const typename addr_t base);
-    VciGcdMaster<vci_param> master("gcd_master", IntTab(0), maptab, seed, GCD_BASE);
-    // VciGcdCoprocessor(sc_module_name insname, const IntTab& index, const MappingTable& mt);
-    VciGcdCoprocessor<vci_param> coproc("gcd_coprocessor", IntTab(0), maptab);
+    // Initiators/Masters
+    VciGcdMaster<vci_param> master_0("gcd_master_0", IntTab(0), maptab, seed, GCD0_BASE);
+    VciGcdMaster<vci_param> master_1("gcd_master_1", IntTab(1), maptab, seed, GCD1_BASE);
+    VciGcdMaster<vci_param> master_2("gcd_master_2", IntTab(2), maptab, seed, GCD2_BASE);
+    // Targets/Slaves
+    VciGcdCoprocessor<vci_param> coproc_0("gcd_coprocessor_0", IntTab(0), maptab);
+    VciGcdCoprocessor<vci_param> coproc_1("gcd_coprocessor_1", IntTab(1), maptab);
+    VciGcdCoprocessor<vci_param> coproc_2("gcd_coprocessor_2", IntTab(2), maptab);
+    // VCI VGSB bus (3 initiators, 3 targets)
+    VciVgsb<vci_param> vci_bus("vci_vgsb", maptab, 3, 3);
 
     //////////////////////////////////////////////////////////////////////////
     // Net-List
     //////////////////////////////////////////////////////////////////////////
-    master.p_clk(signal_clk);
-    master.p_resetn(signal_resetn);
-    master.p_vci(signal_vci);
-
-    coproc.p_clk(signal_clk);
-    coproc.p_resetn(signal_resetn);
-    coproc.p_vci(signal_vci);
+    // Master 0
+    master_0.p_clk(signal_clk);
+    master_0.p_resetn(signal_resetn);
+    master_0.p_vci(signal_vci_m0);
+    // Master 1
+    master_1.p_clk(signal_clk);
+    master_1.p_resetn(signal_resetn);
+    master_1.p_vci(signal_vci_m1);
+    // Master 2
+    master_2.p_clk(signal_clk);
+    master_2.p_resetn(signal_resetn);
+    master_2.p_vci(signal_vci_m2);
+    // Coprocessor 0
+    coproc_0.p_clk(signal_clk);
+    coproc_0.p_resetn(signal_resetn);
+    coproc_0.p_vci(signal_vci_s0);
+    // Coprocessor 1
+    coproc_1.p_clk(signal_clk);
+    coproc_1.p_resetn(signal_resetn);
+    coproc_1.p_vci(signal_vci_s1);
+    // Coprocessor 2
+    coproc_2.p_clk(signal_clk);
+    coproc_2.p_resetn(signal_resetn);
+    coproc_2.p_vci(signal_vci_s2);
+    // VCI VGSB bus
+    vci_bus.p_clk(signal_clk);
+    vci_bus.p_resetn(signal_resetn);
+    // Connect initiators/masters to the bus
+    vci_bus.p_to_initiator[0](signal_vci_m0);
+    vci_bus.p_to_initiator[1](signal_vci_m1);
+    vci_bus.p_to_initiator[2](signal_vci_m2);
+    // Connect targets/slaves to the bus
+    vci_bus.p_to_target[0](signal_vci_s0);
+    vci_bus.p_to_target[1](signal_vci_s1);
+    vci_bus.p_to_target[2](signal_vci_s2);
 
     //////////////////////////////////////////////////////////////////////////
     // simulation
