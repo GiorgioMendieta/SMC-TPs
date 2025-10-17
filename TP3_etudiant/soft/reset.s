9981e045b83e4c64b6b3dc0a39c8c8ba4da59b03
Giorgio Mendieta <carscars21@gmail.com>
2025-10-17 17:12:10 +0200

    Finish reset.s and fix cache coherency variable error

--- TP3_etudiant/soft/reset.s
+++ TP3_etudiant/soft/reset.s
@@ -22,22 +22,22 @@ reset:
     .set noreorder
 
 # initializes stack pointer
-	la	$27,	seg_stack_base
-    li	/* TODO: */		# $26 <= 64K
-	addu	/* TODO: */		# $29 <= seg_stack_base + 64K
+	la	    $27,	seg_stack_base
+    li	    $26,    0x10000		# $26 <= 64K
+	addu	$29, $27, $26		# $29 <= seg_stack_base + 64K
 
 # initializes EPC register   
-	la	$26,	main		# $26 <= address main
+	la	    $26,	main		# $26 <= address main
 	mtc0	$26,	$14		# EPC <= main
 
 # initializes SR register
-    li	$26,	0x0000FF13		
+    li	    $26,	0x0000FF13		
     mtc0	$26,	$12		# SR <= user mode / IRQ enable (after eret)
 
 # jumps to main 
-    la      /* TODO: */         # $26 <= seg_data_base
-    lw      /* TODO: */         # $26 <= M[seg_data_base]
-    mtc0    /* TODO: */         # EPC <= $26  (EPC == $14)
+    la      $26, seg_data_base         # $26 <= seg_data_base
+    lw      $26, 0($26)         # $26 <= M[seg_data_base] NOT SURE
+    mtc0    $26, $14         # EPC <= $26  (EPC == $14)
 	eret
 
 	.end	reset
