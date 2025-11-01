f177655f8d8c6877ac5dc009c3956df1a777bd82
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2025-11-01 14:39:43 +0100

    Add images.raw file

--- TP4_etudiant/soft/main.c
+++ TP4_etudiant/soft/main.c
@@ -9,26 +9,28 @@ __attribute__((constructor)) void main()
     tty_puts("- a : activation des interruptions générées par le TIMER\n");
     tty_puts("- d : desactivation des interruptions générées par le TIMER\n");
     tty_puts("- q : sortie de l'interprêteur par l'appel système `exit()`\n");
-    
+
     while (1)
     {
         tty_getc_irq(&c); // Wait for a character input (blocking)
 
-        if (c == 'd'){
-	    tty_puts("Désactivation des interruptions\n");
+        if (c == 'd')
+        {
+            tty_puts("Désactivation des interruptions\n");
             timer_set_mode(0x0); // Deactivate timer
-	}
-	if (c == 'a'){
-	    tty_puts("Activation des interruptions\n");
+        }
+        if (c == 'a')
+        {
+            tty_puts("Activation des interruptions\n");
             timer_set_mode(0x3); // Activate timer with periodic interrupts
         }
-	if (c == 'q'){
-	    tty_puts("Sortie du programme\n");
+        if (c == 'q')
+        {
+            tty_puts("Sortie du programme\n");
             timer_set_mode(0x0); // Deactivate timer
             exit();
-	}
+        }
     }
 
     exit();
 } // end main
-
