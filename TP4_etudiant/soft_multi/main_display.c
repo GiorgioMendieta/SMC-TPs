06b154b6b740022269af06d03d3a973d29b9c4e9
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2025-11-02 13:00:05 +0100

    Create soft_multi directory

---
+++ TP4_etudiant/soft_multi/main_display.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+
+__attribute__((constructor)) void main()
+{
+    unsigned char tab[128][128]; // Image buffer
+    int index_block = 0;
+    int index_img   = 0;
+    char c;
+    tty_puts("Press any key to load an image from disk\n");
+
+    while (1)
+    {
+        tty_printf("Image Nb: %u\n", index_img);
+        // Wait for a character input (blocking)
+        tty_getc_irq(&c);
+        if (c > 0)
+        {
+            if (ioc_read(index_block, &tab, 32) != 0)
+            {
+                tty_puts("Error during ioc read\n");
+            }
+            if (ioc_completed() != 0)
+            {
+                tty_puts("Error during ioc transfer\n");
+            }
+            if (fb_write(0, &tab, 16384) != 0)
+            {
+                tty_puts("Error during the fbf write\n");
+            }
+        }
+
+        index_block += 32; // Next image
+        ++index_img;
+
+        if (index_img == 21) // Restart from the first image
+        {
+            index_block = 0;
+            index_img   = 0;
+        }
+    }
+
+    exit();
+} // end main
